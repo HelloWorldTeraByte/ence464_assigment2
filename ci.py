4956f1bccc4633bc9f64b3be659d0ccb65aaeb16
Randipa G <randipa99@gmail.com>
2021-10-05 19:43:53 +1300

    Added testing

--- ci.py
+++ ci.py
@@ -1,14 +1,46 @@
 import subprocess
 import time
+import os
 
 build_dir = 'build/'
-n = 101
-i = 300
+n = 51
+iters = 300
+test_ver = 3
 
-start_time = time.time()
-subprocess.run(['./' + build_dir + 'poisson1', '-n', str(n), '-i', str(i)], stdout=subprocess.DEVNULL)
-print('1: ', time.time() - start_time)
+just_test = True
 
-start_time = time.time()
-subprocess.run(['./' + build_dir + 'poisson2', '-n', str(n), '-i', str(i)], stdout=subprocess.DEVNULL)
-print('2: ', time.time() - start_time)
+print('-' * 50)
+print('Testing: poision' + str(test_ver))
+print('-' * 50)
+
+r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 7' + 
+               ' -i 300 ' + ' | cmp reference/7.txt > /dev/null')
+if(r):
+  print('n= 7 i=300 Failed!')
+else:
+  print('n= 7 i=300 Passed!')
+
+r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 15' + 
+               ' -i 300 ' + ' | cmp reference/15.txt > /dev/null')
+
+if(r):
+  print('n= 15 i=300 Failed!')
+else:
+  print('n= 15 i=300 Passed!')
+
+r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 51' + 
+               ' -i 300 ' + ' | cmp reference/51.txt > /dev/null')
+
+if(r):
+  print('n= 51 i=300 Failed!')
+else:
+  print('n= 51 i=300 Passed!')
+
+if(just_test):
+  exit()
+
+for i in range(1, 4):
+  start_time = time.time()
+  subprocess.run(['./' + build_dir + 'poisson' + str(i), '-n', str(n),
+               '-i', str(iters)], stdout=subprocess.DEVNULL)
+  print(i, ': ', time.time() - start_time)
