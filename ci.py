d7912056635e1772ae623d4ce5a7cdbe4b37d893
Randipa G <randipa99@gmail.com>
2021-10-06 12:01:58 +1300

    Bit of cleanup

--- ci.py
+++ ci.py
@@ -3,44 +3,54 @@ import time
 import os
 
 build_dir = 'build/'
+
+vers = 4
+test_ver = 4
 n = 51
 iters = 300
-test_ver = 3
 
-just_test = True
+test = True
+benchmark = False
+#benchmark = True
 
-print('-' * 50)
-print('Testing: poision' + str(test_ver))
-print('-' * 50)
+def Test():
+  print('-' * 50)
+  print('Testing: poision' + str(test_ver))
+  print('-' * 50)
 
-r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 7' + 
-               ' -i 300 ' + ' | cmp reference/7.txt > /dev/null')
-if(r):
-  print('n= 7 i=300 Failed!')
-else:
-  print('n= 7 i=300 Passed!')
+  r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 7' + 
+                 ' -i 300 ' + ' | cmp reference/7.txt > /dev/null')
+  if(r):
+    print('n=7 i=300 Failed!')
+  else:
+    print('n=7 i=300 Passed!')
 
-r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 15' + 
-               ' -i 300 ' + ' | cmp reference/15.txt > /dev/null')
+  r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 15' + 
+                 ' -i 300 ' + ' | cmp reference/15.txt > /dev/null')
 
-if(r):
-  print('n= 15 i=300 Failed!')
-else:
-  print('n= 15 i=300 Passed!')
+  if(r):
+    print('n=15 i=300 Failed!')
+  else:
+    print('n=15 i=300 Passed!')
 
-r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 51' + 
-               ' -i 300 ' + ' | cmp reference/51.txt > /dev/null')
+  r = os.system('./' + build_dir + 'poisson' + str(test_ver) + ' -n 51' + 
+                 ' -i 300 ' + ' | cmp reference/51.txt > /dev/null')
 
-if(r):
-  print('n= 51 i=300 Failed!')
-else:
-  print('n= 51 i=300 Passed!')
+  if(r):
+    print('n=51 i=300 Failed!')
+  else:
+    print('n=51 i=300 Passed!')
 
-if(just_test):
-  exit()
+def Benchmark():
+  for i in range(1, vers + 1):
+    start_time = time.time()
+    subprocess.run(['./' + build_dir + 'poisson' + str(i), '-n', str(n),
+                 '-i', str(iters)], stdout=subprocess.DEVNULL)
+    print(i, ': ', time.time() - start_time)
 
-for i in range(1, 4):
-  start_time = time.time()
-  subprocess.run(['./' + build_dir + 'poisson' + str(i), '-n', str(n),
-               '-i', str(iters)], stdout=subprocess.DEVNULL)
-  print(i, ': ', time.time() - start_time)
+if __name__ == '__main__':
+  if(test):
+    Test()
+
+  if(benchmark):
+    Benchmark()
